b3992579985337fd51d9159089a6dc5a448ff654
elbatouri rabi <elbatouri@gmail.com>
2023-11-01 16:21:15 -0700

    push the correction of task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -23,7 +23,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	}
 	if (parent->right != NULL)
 	{
-		new->right = parent->left;
+		new->right = parent->right;
 		parent->right->parent = new;
 	}
 	parent->right = new;
