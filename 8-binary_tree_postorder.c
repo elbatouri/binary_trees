5a6ed630d0ae53d63f8dc4a94f242fcdd6536abd
elbatouri rabi <elbatouri@gmail.com>
2023-11-01 16:17:49 -0700

    task 0 corecting betty style

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a binary tree 
+ * using post-order traversal
  * @tree: tree to go tverse
- * @func: function
+ * @func: function to traverse
  * Return: Nothing
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
