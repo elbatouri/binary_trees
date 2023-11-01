1c3f16e2d9354f218f514af4fffc00ac97c60d94
elbatouri rabi <elbatouri@gmail.com>
2023-11-01 14:38:29 -0700

    insert left task

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,11 +1,11 @@
-#include "binary_tress.h"
+#include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - add in the left of the parrent a new node 
+ * binary_tree_insert_left - add in the left of the parrent a new node
  * and if the node exist you add the new note to the down level
  * @parent: the parent of the node
  * @value: value in the node
- * Return: NULL if fail
+ * Return: if it fail null if not new node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
@@ -15,20 +15,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	
-	new = binary_tree_node(parent , value);
+
+	new = binary_tree_node(parent, value);
 	if (new == NULL)
 	{
-		new = binary_tree_node(parent, value);
-		if (new == NULL)
-		{
-			return (NULL);
-		}
-		if(parent->left !=NULL)
-		{
-			new->left = parent->left;
-			parent->left > parent
-		}
-		parent->left = new;
-		return (new_node
-}
+		return (NULL);
+	}
+	if (parent->left != NULL)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
+	parent->left = new;
+	return (new);
+	}
