54443f33ae5c765782b061ea8adbbce8fb7cf29c
elbatouri rabi <elbatouri@gmail.com>
2023-11-01 14:54:02 -0700

    insert right task

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,23 +9,23 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new;
+	binary_tree_t *new;
 
-        if (parent == NULL)
-        {
-                return (NULL);
-        }
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 
-        new = binary_tree_node(parent, value);
-        if (new == NULL)
-        {
-                return (NULL);
-        }
-        if (parent->right != NULL)
-        {
-                new->right = parent->left;
-                parent->right->parent = new;
-        }
-        parent->right = new;
-        return (new);
-        }
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	if (parent->right != NULL)
+	{
+		new->right = parent->left;
+		parent->right->parent = new;
+	}
+	parent->right = new;
+	return (new);
+	}
