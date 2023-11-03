0572206449ac34218d63058d73ab6371fc25fdb9
redamjibel <redamjibel@hotmail.com>
2023-11-02 19:43:24 -0700

    update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -36,8 +36,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	leftheight = height(tree->left);
 	rightheight = height(tree->right);
-	if (leftheight == 0)
-		return (0);
 
 	if (leftheight == rightheight)
 	{
